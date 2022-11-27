bcec2d14d9dca2a8f83e1e4f28e3d9673d613187
Ersin İşgör <ersinisgor@hotmail.com>
2022-11-27 20:42:27 +0300

    Update fundamentals-part-1 assignments 1,2,3,4

---
+++ 01-Fundamentals-Part-1/current/assignments.js
@@ -0,0 +1,29 @@
+/* Values and Veriables */
+
+let country = "Turkey";
+let continent = "European continent";
+let population = 85.0e6;
+
+console.log(population);
+
+/* Data Types */
+
+let isIsland = false;
+let language;
+
+/* console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language); */
+
+/* Let, Const and Var */
+language = "Turkish";
+const capital = "Ankara";
+/* console.log(capital); */
+
+/* Basic Operators */
+let split = population / 2;
+split++;
+const finland = 6.0e6;
+const avaragePopulation = 33.0e6;
+/* console.log(population < avaragePopulation); */
