b96803aa07404452b7fbb6ba04cddd7d0d201c6a
Ersin İşgör <ersinisgor@hotmail.com>
2022-12-07 22:24:44 +0300

    Fundamentals-Part-1 assignment 'the switch statement' done

--- 01-Fundamentals-Part-1/current/assignments.js
+++ 01-Fundamentals-Part-1/current/assignments.js
@@ -87,3 +87,26 @@ if (language === "English" && population < 50.0e6 && isIsland) {
 } else {
   console.log(`${country} doesn't meet your criteria`);
 }
+
+/* Switch Statement */
+switch (language.toLowerCase()) {
+  case "chinese":
+  case "mandarin":
+    console.log("MOST number of native speakers!");
+    break;
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "english":
+    console.log("3rd place");
+    break;
+  case "hindi":
+    console.log("Number 4");
+    break;
+  case "arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too :D");
+    break;
+}
