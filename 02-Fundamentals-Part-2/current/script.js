6f1c6348d8fcc7068fb980f478e4ede4706c4bf6
Ersin İşgör <ersinisgor@hotmail.com>
2022-12-25 20:47:31 +0300

    Fundamentals-Part-2 assignments 'Arrow Function' done

--- 02-Fundamentals-Part-2/current/script.js
+++ 02-Fundamentals-Part-2/current/script.js
@@ -23,13 +23,24 @@ const germany = persantageOfWorld1(83);
 console.log(turkey, portugal, germany); */
 
 /* Function Expression */
-const persantageOfWorld2 = function (population) {
+/* const persantageOfWorld2 = function (population) {
   const worldPopulation = 7900;
   return (population * 100) / worldPopulation;
+}; */
+
+/* const turkey = persantageOfWorld2(86);
+const portugal = persantageOfWorld2(10);
+const germany = persantageOfWorld2(83); */
+
+//console.log(turkey, portugal, germany);
+
+/* Arrow Function */
+const persantageOfWorld3 = (population) => {
+  (population * 100) / 7900;
 };
 
-const turkey = persantageOfWorld2(86);
-const portugal = persantageOfWorld2(10);
-const germany = persantageOfWorld2(83);
+const turkey = persantageOfWorld3(86);
+const portugal = persantageOfWorld3(10);
+const germany = persantageOfWorld3(83);
 
 console.log(turkey, portugal, germany);
