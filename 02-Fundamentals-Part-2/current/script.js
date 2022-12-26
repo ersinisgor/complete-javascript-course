46512f72441c845a49488851ac96c45df5182628
Ersin İşgör <ersinisgor@hotmail.com>
2022-12-26 19:41:33 +0300

    Fundamentals-Part-2 assignment 'Functions Calling Other Functions' done

--- 02-Fundamentals-Part-2/current/script.js
+++ 02-Fundamentals-Part-2/current/script.js
@@ -4,17 +4,17 @@ function describeCountry(country, population, capitalCity) {
   return `${country} has ${population} million people and its capital city is ${capitalCity}`;
 }
 
-//const country1 = describeCountry("Turkey", 86, "Ankara");
+const country1 = describeCountry("Turkey", 86, "Ankara");
 //const country2 = describeCountry("Portugal", 10, "Lisbon");
 //const country3 = describeCountry("Germany", 83, "Berlin");
 
 //console.log(country1, country2, country3);
 
 /* Function Declaration */
-/* function persantageOfWorld1(population) {
+function persantageOfWorld1(population) {
   const worldPopulation = 7900;
   return (population * 100) / worldPopulation;
-} */
+}
 
 /* const turkey = persantageOfWorld1(86);
 const portugal = persantageOfWorld1(10);
@@ -35,7 +35,7 @@ const germany = persantageOfWorld2(83); */
 //console.log(turkey, portugal, germany);
 
 /* Arrow Function */
-const persantageOfWorld3 = (population) => {
+/* const persantageOfWorld3 = (population) => {
   (population * 100) / 7900;
 };
 
@@ -43,4 +43,13 @@ const turkey = persantageOfWorld3(86);
 const portugal = persantageOfWorld3(10);
 const germany = persantageOfWorld3(83);
 
-console.log(turkey, portugal, germany);
+console.log(turkey, portugal, germany); */
+
+/* Funcion in Other Function */
+const describePopulation = function (country, population) {
+  const percentage = persantageOfWorld1(population);
+  const description = `${country} has ${population} million people, which is about ${percentage}% of the world`;
+  console.log(description);
+};
+
+describePopulation("Turkey", 86);
