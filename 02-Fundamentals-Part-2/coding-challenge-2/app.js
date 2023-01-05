6248fea4478caf9923cd55bf3aefb77ebff8943d
Ersin İşgör <ersinisgor@hotmail.com>
2023-01-05 11:11:19 +0300

    Fundamentals-Part-2 'Coding-Challenge-2' done

---
+++ 02-Fundamentals-Part-2/coding-challenge-2/app.js
@@ -0,0 +1,9 @@
+let bills = [125, 555, 44];
+
+function calcTip(bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+}
