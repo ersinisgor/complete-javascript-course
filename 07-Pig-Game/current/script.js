59297c691aecf5821cce0402979ad25da34a62ea
Ersin İşgör <ersinisgor@hotmail.com>
2023-01-12 12:38:27 +0300

    Update holdScore and playGame func

--- 07-Pig-Game/current/script.js
+++ 07-Pig-Game/current/script.js
@@ -1,21 +1,37 @@
 'use strict';
 const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector('.btn--new');
 const diceImage = document.querySelector('.dice');
-const player = document.querySelectorAll('.player');
 const currentScore = document.querySelectorAll('.current-score');
 const player1Score = document.querySelector('#score--0');
 const player2Score = document.querySelector('#score--1');
-const btnHold = document.querySelector('.btn--hold');
+const player = document.querySelectorAll('.player');
+
+const player1 = player[0].classList;
+const player2 = player[1].classList;
 
 let value1 = 0;
 let value2 = 0;
 let score1 = 0;
 let score2 = 0;
-currentScore[0].textContent = value1;
-currentScore[1].textContent = value2;
-player1Score.textContent = score1;
-player2Score.textContent = score2;
+let isThereWinner = false;
 diceImage.style.display = 'none';
+
+const reset = function () {
+  debugger;
+  value1 = 0;
+  value2 = 0;
+  score1 = 0;
+  score2 = 0;
+  currentScore[0].textContent = value1;
+  currentScore[1].textContent = value2;
+  player1Score.textContent = score1;
+  player2Score.textContent = score2;
+  //diceImage.style.display = 'none';
+  player1.remove('player--winner');
+  player2.remove('player--winner');
+};
 
 const randomNumber = function () {
   const random = Math.trunc(Math.random() * 6) + 1;
@@ -23,85 +39,113 @@ const randomNumber = function () {
 };
 
 const switchPlayer = function () {
-  if (player[0].classList.contains('player--active')) {
-    player[0].classList.remove('player--active');
-    player[1].classList.add('player--active');
-    diceImage.style.display = 'none';
-    currentScore[0].textContent = 0;
-    currentScore[1].textContent = 0;
+  if (player1.contains('player--active')) {
+    player1.remove('player--active');
+    player2.add('player--active');
     value1 = 0;
-    //player1Score.textContent = 0;
-  } else if (player[1].classList.contains('player--active')) {
-    player[1].classList.remove('player--active');
-    player[0].classList.add('player--active');
-    diceImage.style.display = 'none';
-    currentScore[1].textContent = 0;
-    currentScore[0].textContent = 0;
+    currentScore[0].textContent = value1;
+  } else if (player2.contains('player--active')) {
+    player2.remove('player--active');
+    player1.add('player--active');
     value2 = 0;
-    //player2Score.textContent = 0;
+    currentScore[1].textContent = value2;
   }
 };
 
 const holdScore = function () {
-  //debugger;
-  if (player[0].classList.contains('player--active')) {
-    //value += currentScore[0].textContent;
-    //player1Score.textContent = currentScore[0].textContent + value;
-    score1 += Number(currentScore[0].textContent);
+  if (player1.contains('player--active') && !isThereWinner) {
+    score1 += value1;
     player1Score.textContent = score1;
-    switchPlayer();
-  } else if (player[1].classList.contains('player--active')) {
-    //player2Score.textContent = currentScore[1].textContent + value;
-    //value += currentScore[1].textContent;
-    score2 += Number(currentScore[1].textContent);
+    if (score1 < 6) {
+      value1 = 0;
+      switchPlayer();
+    } else {
+      player1.add('player--winner');
+      isThereWinner = true;
+    }
+  } else if (player2.contains('player--active') && !isThereWinner) {
+    score2 += value2;
     player2Score.textContent = score2;
-    switchPlayer();
+    if (score2 < 6) {
+      value2 = 0;
+      switchPlayer();
+    } else {
+      player2.add('player--winner');
+      isThereWinner = true;
+    }
   }
 };
 
 const playGame = function () {
-  let dice = randomNumber();
-  diceImage.style.display = 'block';
-  console.log(dice);
+  if (!isThereWinner) {
+    let dice = randomNumber();
 
-  switch (dice) {
-    case 1:
-      diceImage.setAttribute('src', 'dice-1.png');
-      break;
-    case 2:
-      diceImage.setAttribute('src', 'dice-2.png');
-      break;
-    case 3:
-      diceImage.setAttribute('src', 'dice-3.png');
-      break;
-    case 4:
-      diceImage.setAttribute('src', 'dice-4.png');
-      break;
-    case 5:
-      diceImage.setAttribute('src', 'dice-5.png');
-      break;
-    case 6:
-      diceImage.setAttribute('src', 'dice-6.png');
-      break;
-    default:
-      diceImage.setAttribute('src', '');
-      break;
-  }
-
-  setTimeout(function () {
-    if (dice === 1) {
-      switchPlayer();
-    } else {
-      if (player[0].classList.contains('player--active')) {
-        value1 += dice;
-        currentScore[0].textContent = value1;
-      } else if (player[1].classList.contains('player--active')) {
-        value2 += dice;
-        currentScore[1].textContent = value2;
-      }
+    switch (dice) {
+      case 1:
+        diceImage.style.display = 'block';
+        diceImage.setAttribute('src', 'dice-1.png');
+        setTimeout(() => {
+          diceImage.style.display = 'none';
+        }, 800);
+        break;
+      case 2:
+        diceImage.style.display = 'block';
+        diceImage.setAttribute('src', 'dice-2.png');
+        setTimeout(() => {
+          diceImage.style.display = 'none';
+        }, 800);
+        break;
+      case 3:
+        diceImage.style.display = 'block';
+        diceImage.setAttribute('src', 'dice-3.png');
+        setTimeout(() => {
+          diceImage.style.display = 'none';
+        }, 800);
+        break;
+      case 4:
+        diceImage.style.display = 'block';
+        diceImage.setAttribute('src', 'dice-4.png');
+        setTimeout(() => {
+          diceImage.style.display = 'none';
+        }, 800);
+        break;
+      case 5:
+        diceImage.style.display = 'block';
+        diceImage.setAttribute('src', 'dice-5.png');
+        setTimeout(() => {
+          diceImage.style.display = 'none';
+        }, 800);
+        break;
+      case 6:
+        diceImage.style.display = 'block';
+        diceImage.setAttribute('src', 'dice-6.png');
+        setTimeout(() => {
+          diceImage.style.display = 'none';
+        }, 800);
+        break;
+      default:
+        diceImage.style.display = 'none';
+        break;
     }
-  }, 500);
+
+    if (dice === 1) {
+      setTimeout(() => {
+        switchPlayer();
+      }, 800);
+    } else {
+      setTimeout(() => {
+        if (player1.contains('player--active')) {
+          value1 += dice;
+          currentScore[0].textContent = value1;
+        } else if (player2.contains('player--active')) {
+          value2 += dice;
+          currentScore[1].textContent = value2;
+        }
+      }, 500);
+    }
+  }
 };
 
 btnRoll.addEventListener('click', playGame);
 btnHold.addEventListener('click', holdScore);
+btnNew.addEventListener('click', reset);
